641a841632c55388d4563f8d539258c35e1c9aed
Jan Bartnicki <jan.bartnicki01@gmail.com>
2025-04-30 13:34:57 +0200

    Base routing - home page

--- Quiz/App.xaml.cs
+++ Quiz/App.xaml.cs
@@ -1,13 +1,19 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Quiz.ViewModels;
 using System.Windows;
 
 namespace Quiz;
-
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        MainWindow = new MainWindow()
+        {
+            DataContext = new MainViewModel()
+        };
+        MainWindow.Show();
+           
+
+        base.OnStartup(e);
+    }
 }
 
