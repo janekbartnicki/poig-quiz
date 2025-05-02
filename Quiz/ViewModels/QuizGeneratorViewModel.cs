f69f6b2a2d8823c485425c6c7da37437e82ed2a2
Jan Bartnicki <jan.bartnicki01@gmail.com>
2025-05-02 16:56:50 +0200

    Basic quiz generator navigation

--- Quiz/ViewModels/QuizGeneratorViewModel.cs
+++ Quiz/ViewModels/QuizGeneratorViewModel.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Quiz.Commands;
+using Quiz.Stores;
 
 namespace Quiz.ViewModels
 {
     class QuizGeneratorViewModel : ViewModelBase
     {
+        public ICommand NavigateHomeCommand { get; }
+
+        public QuizGeneratorViewModel(NavigationStore navigationStore)
+        {
+            NavigateHomeCommand = new NavigateHomeCommand(navigationStore);
+        }
     }
 }
