0d296131d5844b80cd1805803c42e51d8d104dd2
Jan Bartnicki <jan.bartnicki01@gmail.com>
2025-05-03 20:02:54 +0200

    Quiz list in quiz generator

--- Quiz/Models/Quiz.cs
+++ Quiz/Models/Quiz.cs
@@ -9,6 +9,12 @@ namespace Quiz.Models
     public class Quiz
     {
         public string Title { get; set; }
-        public List<Question> questions { get; set; }
+        public List<Question> Questions { get; set; }
+
+        public Quiz(string title, List<Question> questions)
+        {
+            Title = title;
+            Questions = questions;
+        }
     }
 }
