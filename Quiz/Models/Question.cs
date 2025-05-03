a106e5e7f652e20c3cf46f689e6d45ba96999550
Jan Bartnicki <jan.bartnicki01@gmail.com>
2025-05-03 15:46:09 +0200

    Saving quiz to JSON with AES

---
+++ Quiz/Models/Question.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz.Models
+{
+    public class Question
+    {
+        public string Text { get; set; }
+        public string CorrectAnswer { get; set; }
+        public List<string> Questions { get; set; }
+
+        public bool IsAnswerCorrect(string answer)
+        {
+            return answer == CorrectAnswer;
+        }
+    }
+}
