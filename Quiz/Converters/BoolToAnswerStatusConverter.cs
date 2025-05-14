18327e32ce0f4ebcf4a215fdf26c53e640f439ed
domelexe <dominik.domel.bienia@gmail.com>
2025-05-14 05:20:54 +0200

    Kolejne poprawki

---
+++ Quiz/Converters/BoolToAnswerStatusConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Quiz.Converters
+{
+    public class BoolToAnswerStatusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isCorrect = (bool)value;
+            return isCorrect ? "Poprawna odpowiedź" : "Niepoprawna odpowiedź";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Quiz/ViewModels/QuizViewModel.cs
+++ Quiz/ViewModels/QuizViewModel.cs
@@ -1,12 +1,354 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
-using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+using Microsoft.Win32;
+using Quiz.Commands;
+using Quiz.Models;
+using Quiz.ViewModels;
 
 namespace Quiz.ViewModels
 {
-    class QuizViewModel : ViewModelBase
+    public class QuizViewModel : ViewModelBase
     {
+        private Models.Quiz _currentQuiz;
+        private int _currentQuestionIndex;
+        private ObservableCollection<bool> _selectedAnswers;
+        private List<List<bool>> _allUserAnswers = new List<List<bool>>();
+        private DispatcherTimer _timer;
+        private int _timeLeft = 30;
+        private bool _isQuizLoaded = false;
+        private bool _isQuizFinished = false;
+        private bool _isTimerRunning = false;
+        private int _correctAnswersCount = 0;
+
+        // Properties for binding
+        public Models.Quiz CurrentQuiz
+        {
+            get { return _currentQuiz; }
+            set
+            {
+                if (_currentQuiz != value)
+                {
+                    _currentQuiz = value;
+                    OnPropertyChanged(nameof(CurrentQuiz));
+                    OnPropertyChanged(nameof(TotalQuestions));
+                }
+            }
+        }
+
+        public Question CurrentQuestion
+        {
+            get
+            {
+                return _currentQuestionIndex < CurrentQuiz?.Questions.Count ?
+                CurrentQuiz.Questions[_currentQuestionIndex] : null;
+            }
+        }
+
+        public int CurrentQuestionIndex
+        {
+            get { return _currentQuestionIndex; }
+            set
+            {
+                if (_currentQuestionIndex != value)
+                {
+                    _currentQuestionIndex = value;
+                    OnPropertyChanged(nameof(CurrentQuestionIndex));
+                    OnPropertyChanged(nameof(CurrentQuestion));
+                    OnPropertyChanged(nameof(QuestionNumber));
+                }
+            }
+        }
+
+        public int QuestionNumber
+        {
+            get { return _currentQuestionIndex + 1; }
+        }
+
+
+        public int TotalQuestions => CurrentQuiz?.Questions.Count ?? 0;
+
+        public ObservableCollection<bool> SelectedAnswers
+        {
+            get { return _selectedAnswers; }
+            set
+            {
+                _selectedAnswers = value;
+                OnPropertyChanged(nameof(SelectedAnswers));
+            }
+        }
+
+        public int TimeLeft
+        {
+            get { return _timeLeft; }
+            set
+            {
+                if (_timeLeft != value)
+                {
+                    _timeLeft = value;
+                    OnPropertyChanged(nameof(TimeLeft));
+                }
+            }
+        }
+
+        public bool IsQuizLoaded
+        {
+            get { return _isQuizLoaded; }
+            set
+            {
+                if (_isQuizLoaded != value)
+                {
+                    _isQuizLoaded = value;
+                    OnPropertyChanged(nameof(IsQuizLoaded));
+                }
+            }
+        }
+
+        public bool IsQuizFinished
+        {
+            get { return _isQuizFinished; }
+            set
+            {
+                if (_isQuizFinished != value)
+                {
+                    _isQuizFinished = value;
+                    OnPropertyChanged(nameof(IsQuizFinished));
+                }
+            }
+        }
+
+        public int CorrectAnswersCount
+        {
+            get { return _correctAnswersCount; }
+            set
+            {
+                if (_correctAnswersCount != value)
+                {
+                    _correctAnswersCount = value;
+                    OnPropertyChanged(nameof(CorrectAnswersCount));
+                    OnPropertyChanged(nameof(ScorePercentage));
+                }
+            }
+        }
+
+        public int ScorePercentage => TotalQuestions > 0 ? (CorrectAnswersCount * 100) / TotalQuestions : 0;
+
+        public ObservableCollection<QuestionResultViewModel> QuizResults { get; } = new ObservableCollection<QuestionResultViewModel>();
+
+        // Commands
+        public ICommand LoadQuizCommand { get; }
+        public ICommand NextQuestionCommand { get; }
+        public ICommand RestartQuizCommand { get; }
+
+        public QuizViewModel()
+        {
+            LoadQuizCommand = new RelayCommand(_ => LoadQuiz());
+            NextQuestionCommand = new RelayCommand(_ => NextQuestion(), _ => CanGoToNextQuestion());
+            RestartQuizCommand = new RelayCommand(_ => RestartQuiz());
+
+            InitializeTimer();
+        }
+
+        private void InitializeTimer()
+        {
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            TimeLeft--;
+
+            if (TimeLeft <= 0)
+            {
+                StopTimer();
+                // Auto-submit the current question when time is up
+                NextQuestion();
+            }
+        }
+
+        private void StartTimer()
+        {
+            if (!_isTimerRunning)
+            {
+                TimeLeft = 30; // Reset to 30 seconds
+                _timer.Start();
+                _isTimerRunning = true;
+            }
+        }
+
+        private void StopTimer()
+        {
+            if (_isTimerRunning)
+            {
+                _timer.Stop();
+                _isTimerRunning = false;
+            }
+        }
+
+        private bool CanGoToNextQuestion()
+        {
+            return IsQuizLoaded && !IsQuizFinished;
+        }
+
+        private void LoadQuiz()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Quiz Files (*.json)|*.json|All Files (*.*)|*.*",
+                Title = "Wybierz plik z quizem"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    string filePath = openFileDialog.FileName;
+                    string json = File.ReadAllText(filePath);
+                    CurrentQuiz = JsonSerializer.Deserialize<Models.Quiz>(json);
+
+                    // Reset quiz state
+                    CurrentQuestionIndex = 0;
+                    _allUserAnswers.Clear();
+                    QuizResults.Clear();
+                    CorrectAnswersCount = 0;
+                    IsQuizLoaded = true;
+                    IsQuizFinished = false;
+
+                    // Initialize answers for the first question
+                    InitializeAnswersForCurrentQuestion();
+
+                    // Start the timer
+                    StartTimer();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Wystąpił błąd podczas wczytywania pliku: {ex.Message}", "Błąd",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void InitializeAnswersForCurrentQuestion()
+        {
+            if (CurrentQuestion != null)
+            {
+                SelectedAnswers = new ObservableCollection<bool>(
+                    Enumerable.Repeat(false, CurrentQuestion.Questions.Count));
+                OnPropertyChanged(nameof(CurrentQuestion));
+            }
+        }
+
+        private void NextQuestion()
+        {
+            // Save current answers
+            if (CurrentQuestion != null && SelectedAnswers != null)
+            {
+                List<bool> currentAnswers = SelectedAnswers.ToList();
+                _allUserAnswers.Add(currentAnswers);
+
+                // Calculate if answer is correct
+                List<string> selectedAnswerTexts = new List<string>();
+                for (int i = 0; i < CurrentQuestion.Questions.Count; i++)
+                {
+                    if (SelectedAnswers[i])
+                    {
+                        selectedAnswerTexts.Add(CurrentQuestion.Questions[i]);
+                    }
+                }
+
+                bool isCorrect = CurrentQuestion.IsAnswerCorrect(selectedAnswerTexts);
+                if (isCorrect)
+                {
+                    CorrectAnswersCount++;
+                }
+            }
+
+            StopTimer();
+
+            // Move to next question or finish quiz
+            if (CurrentQuestionIndex < TotalQuestions - 1)
+            {
+                CurrentQuestionIndex++;
+                InitializeAnswersForCurrentQuestion();
+                StartTimer();
+            }
+            else
+            {
+                FinishQuiz();
+            }
+        }
+
+        private void FinishQuiz()
+        {
+            IsQuizFinished = true;
+            StopTimer();
+            PrepareResults();
+        }
+
+        private void PrepareResults()
+        {
+            QuizResults.Clear();
+
+            for (int i = 0; i < CurrentQuiz.Questions.Count; i++)
+            {
+                var question = CurrentQuiz.Questions[i];
+                List<bool> userAnswers = i < _allUserAnswers.Count ? _allUserAnswers[i] : new List<bool>();
+
+                List<string> selectedAnswers = new List<string>();
+                for (int j = 0; j < question.Questions.Count && j < userAnswers.Count; j++)
+                {
+                    if (userAnswers[j])
+                    {
+                        selectedAnswers.Add(question.Questions[j]);
+                    }
+                }
+
+                bool isCorrect = question.IsAnswerCorrect(selectedAnswers);
+
+                QuizResults.Add(new QuestionResultViewModel
+                {
+                    QuestionText = question.Text,
+                    UserAnswers = selectedAnswers,
+                    CorrectAnswers = question.CorrectAnswers,
+                    IsCorrect = isCorrect
+                });
+            }
+        }
+
+        private void RestartQuiz()
+        {
+            if (CurrentQuiz != null)
+            {
+                // Reset quiz state
+                CurrentQuestionIndex = 0;
+                _allUserAnswers.Clear();
+                QuizResults.Clear();
+                CorrectAnswersCount = 0;
+                IsQuizFinished = false;
+
+                // Initialize answers for the first question
+                InitializeAnswersForCurrentQuestion();
+
+                // Start the timer
+                StartTimer();
+            }
+        }
+    }
+
+    public class QuestionResultViewModel : ViewModelBase
+    {
+        public string QuestionText { get; set; }
+        public List<string> UserAnswers { get; set; }
+        public List<string> CorrectAnswers { get; set; }
+        public bool IsCorrect { get; set; }
     }
 }
